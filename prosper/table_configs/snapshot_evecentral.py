d32c16a9de3975dd08d38df62776076a56962c7b
Lockefox <locke.renard@gmail.com>
2016-09-06 12:56:02 -0700

    adding more skelleton framework for tracking table configs (py-per-table)

---
+++ prosper/table_configs/snapshot_evecentral.py
@@ -0,0 +1,21 @@
+'''snapshot_evecentral.py: contains connection logic for snapshot_evecentral database'''
+
+from os import path
+
+import MySQLdb
+
+from prosper.common.utilities import get_config, create_logger
+import prosper.warehouse.Connection as Connection
+
+HERE = path.abspath(path.dirname(__file__))
+CONFIG_ABSPATH = path.join(HERE, 'table_config.cfg')
+
+config = get_config(CONFIG_ABSPATH)
+
+class snapshot_evecentral(Connection.Timeseries):
+    '''worker class for handling eve_central data'''
+    pass
+    #TODO: maybe too complicated
+
+if __name__ == '__main__':
+    print(__name__)
