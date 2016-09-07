0fe392239017d471d305817e531da02f0304a2f4
Lockefox <locke.renard@gmail.com>
2016-09-06 18:04:56 -0700

    adding more framework for default Connection object

--- prosper/table_configs/snapshot_evecentral.py
+++ prosper/table_configs/snapshot_evecentral.py
@@ -3,19 +3,24 @@
 from os import path
 
 import pandas
+import mysql.connector
 
 from prosper.common.utilities import get_config, create_logger
 import prosper.warehouse.Connection as Connection
 
 HERE = path.abspath(path.dirname(__file__))
+ME = __file__.replace('.py', '')
 CONFIG_ABSPATH = path.join(HERE, 'table_config.cfg')
 
 config = get_config(CONFIG_ABSPATH)
+CONNECTION_VALUES = Connection.get_config_values(config, ME)
 
-class snapshot_evecentral(Connection.Timeseries):
+DEBUG = False
+class snapshot_evecentral(Connection.SQLTable):
     '''worker class for handling eve_central data'''
     pass
     #TODO: maybe too complicated
 
 if __name__ == '__main__':
-    print(__name__)
+    print(ME)
+    DEBUG = True
--- prosper/warehouse/Connection.py
+++ prosper/warehouse/Connection.py
@@ -2,22 +2,25 @@
 
 import abc
 import importlib.util
+import configparser
 
 #from prosper.common.utilities import get_config
-def bool_can_write(DatabaseClass):
-    '''return permissions if writing to db is allowed'''
-    pass
-
 class Database(metaclass=abc.ABCMeta):
     '''parent class for holding database connection info'''
     def __init__(self, datasource_name):
         '''basic info about all databases'''
         self.datasource_name = datasource_name
-        self.connection = None
-        self.cursor = None
+        self.connection = self.get_connection()
+        self.cursor = self.get_cursor()
+        #TODO: con/cur method only works for direct db, not RESTy
 
         self.primary_keys = []
         self.data_keys = []
+
+        try:
+            self.test_table()
+        except Exception as error_msg:
+            raise error_msg
 
     def __str__(self):
         return self.datasource_name
@@ -44,7 +47,40 @@ class Database(metaclass=abc.ABCMeta):
         '''save data to table (if allowed)'''
         pass
 
+    @abc.abstractmethod
+    def test_table(self):
+        '''validate that datasource exists, and is ready for data'''
+        pass
+
 class TimeSeries(Database):
     '''child class for handling TimeSeries databases'''
     pass
     #TODO: write helper methods for handling timeseries data
+
+class ConnectionException(Exception):
+    '''base class for table-connection exceptions'''
+    def __init__(self, message, tablename):
+        self.message = message
+        self.tablename = tablename
+
+    def __str__(self):
+        error_msg = 'CONNECTION EXCEPTION: {tablename}-{message}'.\
+        format(
+            tablename=self.tablename,
+            message=self.message
+        )
+        return error_msg
+
+class CreateTableError(Exception):
+    '''unable to create table (if none existed)'''
+    pass
+
+
+def bool_can_write(DatabaseClass):
+    '''return permissions if writing to db is allowed'''
+    pass
+
+def get_config_values(config_object, key_name):
+    '''parses standardized config object and returns vals, or defaults'''
+    pass
+
