902ad14c1060d9627b7bdcb8682d10fd6e542ddb
Lockefox <locke.renard@gmail.com>
2016-09-06 21:48:46 -0700

    filling out abstract methods for snapshot_evecentral

--- prosper/table_configs/snapshot_evecentral.py
+++ prosper/table_configs/snapshot_evecentral.py
@@ -18,7 +18,58 @@ CONNECTION_VALUES = Connection.get_config_values(config, ME)
 DEBUG = False
 class snapshot_evecentral(Connection.SQLTable):
     '''worker class for handling eve_central data'''
-    pass
+    def get_keys(self):
+        '''get primary/data keys from config file'''
+        tmp_primary_keys = []
+        tmp_data_keys = []
+
+        try:
+            tmp_primary_keys = ','.split(config.get(ME, 'primary_keys'))
+            tmp_data_keys = ','.split(config.get(ME, 'data_keys'))
+            self.index_key = config.get(ME, 'index_key') #TODO: this is bad
+        except KeyError as error_msg:
+            raise Connection.TableKeysMissing(
+                error_msg,
+                ME
+            )
+
+        return tmp_primary_keys, tmp_data_keys
+
+    def get_connection(self):
+        '''get con/cur for db connections'''
+        tmp_connection = mysql.connector.connect(
+            user    =CONNECTION_VALUES['user'],
+            password=CONNECTION_VALUES['passwd'],
+            database=CONNECTION_VALUES['schema'],
+            host    =CONNECTION_VALUES['host'],
+            port    =CONNECTION_VALUES['port']
+        )
+        tmp_cursor = tmp_connection.cursor()
+        self.table_name = CONNECTION_VALUES['table'] #TODO: this is bad
+
+        return tmp_connection, tmp_cursor
+
+    def test_table(self):
+        '''test table connection/contents'''
+        #check headers
+        all_keys = []
+        all_keys.append(self.index_key)
+        all_keys.append(self.primary_keys)
+        all_keys.append(self.data_keys)
+
+        header_query = \
+        '''SELECT `COLUMN_NAME`
+            FROM `INFORMATION_SCHEMA`.`COLUMNS`
+            WHERE `TABLE_SCHEMA`='{schema_name}'
+            AND `TABLE_NAME`='{table_name}'''.\
+            format(
+                schema_name=CONNECTION_VALUES['schema'],
+                table_name =CONNECTION_VALUES['table']
+            )
+        self.cursor.execute(header_query)
+        headers = self.cursor.fetchall()
+        if DEBUG:
+            print(headers)
     #TODO: maybe too complicated
 
 if __name__ == '__main__':
--- prosper/warehouse/Connection.py
+++ prosper/warehouse/Connection.py
@@ -10,13 +10,12 @@ class Database(metaclass=abc.ABCMeta):
     def __init__(self, datasource_name):
         '''basic info about all databases'''
         self.datasource_name = datasource_name
-        self.connection = self.get_connection()
-        self.cursor = self.get_cursor()
+        self.table_name = ''
+        self.connection,self.cursor = self.get_connection()
         #TODO: con/cur method only works for direct db, not RESTy
 
-        self.primary_keys = []
-        self.data_keys = []
-
+        self.primary_keys, self.data_keys = self.get_keys()
+        self.index_key = None
         try:
             self.test_table()
         except Exception as error_msg:
@@ -26,13 +25,13 @@ class Database(metaclass=abc.ABCMeta):
         return self.datasource_name
 
     @abc.abstractmethod
-    def get_connection(self):
-        '''get con[nection] for database handles'''
+    def get_keys(self):
+        '''get primary/data keys for query manipulation'''
         pass
 
     @abc.abstractmethod
-    def get_cursor(self):
-        '''get cur[sor] for database handles'''
+    def get_connection(self):
+        '''get con[nection] for database handles'''
         pass
 
     @abc.abstractmethod
@@ -50,6 +49,12 @@ class Database(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def test_table(self):
         '''validate that datasource exists, and is ready for data'''
+        pass
+
+    @abc.abstractmethod
+    def create_table(self):
+        '''create the table if needed'''
+        #TODO: required?
         pass
 
 class SQLTable(Database):
@@ -75,6 +80,10 @@ class CreateTableError(Exception):
     '''unable to create table (if none existed)'''
     pass
 
+class TableKeysMissing(Exception):
+    '''missing keys in config.  Can recreate from db connection...
+        but manual listing for easier code for now'''
+    pass
 
 def bool_can_write(DatabaseClass):
     '''return permissions if writing to db is allowed'''
@@ -83,25 +92,25 @@ def bool_can_write(DatabaseClass):
 def get_config_values(config_object, key_name):
     '''parses standardized config object and returns vals, or defaults'''
     connection_values = {}
-    connection_values['db_schema'] = config_object.get(key_name, 'db_schema')
-    connection_values['db_host']   = config_object.get(key_name, 'db_host')
-    connection_values['db_user']   = config_object.get(key_name, 'db_user')
-    connection_values['db_pw']     = config_object.get(key_name, 'db_pw')
-    connection_values['db_port']   = int(config_object.get(key_name, 'db_port'))
-    connection_values['table_name']= config_object.get(key_name, 'table_name')
+    connection_values['schema'] = config_object.get(key_name, 'db_schema')
+    connection_values['host']   = config_object.get(key_name, 'db_host')
+    connection_values['user']   = config_object.get(key_name, 'db_user')
+    connection_values['passwd'] = config_object.get(key_name, 'db_pw')
+    connection_values['port']   = int(config_object.get(key_name, 'db_port'))
+    connection_values['table']  = config_object.get(key_name, 'table_name')
 
-    if bool(connection_values['db_schema']) and \
-       bool(connection_values['db_host'])   and \
-       bool(connection_values['db_user'])   and \
-       bool(connection_values['db_pw'])     and \
-       bool(connection_values['table_name'])and \
-       bool(connection_values['db_port']):
+    if bool(connection_values['schema']) and \
+       bool(connection_values['host'])   and \
+       bool(connection_values['user'])   and \
+       bool(connection_values['passwd']) and \
+       bool(connection_values['table'])  and \
+       bool(connection_values['port']):
         #if (ANY) blank, use defaults
-        connection_values['db_schema'] = config_object.get('default', 'db_schema')
-        connection_values['db_host']   = config_object.get('default', 'db_host')
-        connection_values['db_user']   = config_object.get('default', 'db_user')
-        connection_values['db_pw']     = config_object.get('default', 'db_pw')
-        connection_values['db_port']   = int(config_object.get('default', 'db_port'))
-        connection_values['table_name']= key_name
+        connection_values['schema'] = config_object.get('default', 'db_schema')
+        connection_values['host']   = config_object.get('default', 'db_host')
+        connection_values['user']   = config_object.get('default', 'db_user')
+        connection_values['passwd'] = config_object.get('default', 'db_pw')
+        connection_values['port']   = int(config_object.get('default', 'db_port'))
+        connection_values['table']  = key_name
 
     return connection_values
