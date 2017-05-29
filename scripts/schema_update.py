0183460b6dfb866d64a6dd0e22c12d2ced12429d
Lockefox <locke.renard@gmail.com>
2017-05-29 15:59:34 -0700

    adding basic frame for schema_update script

---
+++ scripts/schema_update.py
@@ -0,0 +1,48 @@
+"""schema_update.py: helper script for updating MongoDB with new schemas"""
+
+from os import path
+
+import semantic_version
+from pymongo import MongoClient
+from plumbum import cli
+
+import prosper.common.prosper_logging as p_logging
+import prosper.common.prosper_config as p_config
+
+HERE = path.abspath(path.dirname(__file__))
+ROOT = path.dirname(HERE)
+
+CONFIG_PATH = path.join(ROOT, 'prosper', 'warehouse', 'warehouse.cfg')
+CONFIG = p_config.ProsperConfig(CONFIG_PATH)
+
+class UpdateSchemas(cli.Application):
+    """application for updating schemas in production mongo client"""
+    __log_builder = p_logging.ProsperLogging(
+        'UpdateSchemas',
+        HERE,
+        CONFIG
+    )
+
+    debug = cli.Flag(
+        ['d', '--debug'],
+        help='debug mode: do not write to live database'
+    )
+    @cli.switch(
+        ['v', '--verbose'],
+        help='Enable verbose messaging'
+    )
+    def enable_verbose(self):
+        """toggle verbose/stdout logger"""
+        self.__log_builder.configure_debug_logger()
+
+    def main(self):
+        """core logic goes here"""
+        if not self.debug:
+            self.__log_builder.configure_discord_logger()
+
+        logger = self.__log_builder.logger
+
+        logger.info('HELLO WORLD')
+
+if __name__ == '__main__':
+    UpdateSchemas.run()
