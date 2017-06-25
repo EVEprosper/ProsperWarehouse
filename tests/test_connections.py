c1f1e1f4b5c030b8fdc284d3c9bac9054bdc6b70
lockefox <locke.renard@gmail.com>
2017-06-25 12:41:05 -0700

    adding basic test for connection

--- tests/test_connections.py
+++ tests/test_connections.py
@@ -3,9 +3,29 @@
 from os import path
 
 import pytest
+import helpers
 
 import prosper.warehouse.connections as connections
 import prosper.warehouse.exceptions as exceptions
 
 HERE = path.abspath(path.dirname(__file__))
 ROOT = path.dirname(HERE)
+
+ROOT_CONFIG = helpers.load_config(
+    path.join(ROOT, 'prosper', 'warehouse', 'warehouse.cfg')
+)
+TEST_CONFIG = helpers.load_config(
+    path.join(HERE, 'test_config.cfg')
+)
+
+@pytest.mark.mongo
+def test_prod_mongo_happypath():
+    """test production normal path for connections"""
+    test_collection = TEST_CONFIG.get('MONGO', 'test_collection')
+    prod_connection = connections.ProsperWarehouse(test_collection)
+
+    with prod_connection as mongo_handle:
+        test_data = mongo_handle.find_one({})
+
+    assert isinstance(test_data, dict)
+    #TODO: assert test connection has data
